f20e45564afea613811acb0171b18c4d35d2641b
Rochel Yarmark <rochely238@gmail.com>
2022-07-22 13:00:48 -0400

    update

--- 64/jsOne.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var name = "Rochel Yarmark ";
-
-var email = "rochely238@gmail.com";
--- 64/jsTwo.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-console.log(name, email);
-
-var userName = prompt("What is your name?");
-alert("Your name is " + userName);
-console.log("Name: " + userName);
