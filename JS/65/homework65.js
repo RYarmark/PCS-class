603468cd99dba8901c05bbe04ee246951f785d08
Rochel Yarmark <rochely238@gmail.com>
2022-07-25 14:41:26 -0400

    Homework 65

---
+++ JS/65/homework65.js
@@ -0,0 +1,14 @@
+'use strick';
+
+let x = "hello";
+const y = "HELLO";
+console.log(x !== y);
+
+let i = 7;
+let s = "7";
+console.log(i === s);
+
+let z = 'blue' - 2;
+if (isNaN(z)) {
+    console.log("Z is a NaN");
+}
